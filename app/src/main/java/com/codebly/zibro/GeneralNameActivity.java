2ba2fc8c94e613609f4619891ec44d657577b772
comso <>
2024-09-09 12:37:26 +0900

    기본설정

---
+++ app/src/main/java/com/codebly/zibro/GeneralNameActivity.java
@@ -0,0 +1,24 @@
+package com.codebly.zibro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GeneralNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_nickname);
+        ImageView img3 = (ImageView) findViewById(R.id.imageView3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GeneralNameActivity.this, GeneralBirthDateActivity.class));
+            }
+        });
+    }
+}
