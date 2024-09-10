8a0c10d86fe1a74ac0351f7796541f76347dd5df
hanbin4284 <hanbin4284@naver.com>
2024-09-10 17:05:40 +0900

    로그인화면 회원가입화면으로 연결

--- app/src/main/java/com/codebly/zibro/view/login/activity_login.java
+++ app/src/main/java/com/codebly/zibro/view/login/activity_login.java
@@ -1,5 +1,6 @@
 package com.codebly.zibro.view.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,11 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 import com.codebly.zibro.R;
+import com.codebly.zibro.view.home.menu.friends.activity_search_friend;
 
 public class activity_login extends AppCompatActivity {
-
+ImageView imageView13;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,15 @@ public class activity_login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView btnGoToSecondActivity = findViewById(R.id.imageView13);
+
+        imageView13 = (ImageView)findViewById(R.id.imageView13);
+        imageView13.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+
         });
     }
 
