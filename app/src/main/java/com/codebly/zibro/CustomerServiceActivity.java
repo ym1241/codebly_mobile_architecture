3f55a05286592ec88bda267adb885d6d73f071f8
YM Kim <m0m32929@naver.com>
2024-09-11 15:32:18 +0900

    툴바구성중

---
+++ app/src/main/java/com/codebly/zibro/CustomerServiceActivity.java
@@ -0,0 +1,33 @@
+package com.codebly.zibro;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class CustomerServiceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customerservice2);
+
+        Toolbar toolbar = new Toolbar(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
